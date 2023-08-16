4ef735e43de6d79b2f354b8b50efea1c4548f6ac
Cihan Andac <cihanandac@gmail.com>
2023-08-16 14:07:26 +0200

    chores

--- backend/src/rietveld/src/rietveld/content/object.py
+++ backend/src/rietveld/src/rietveld/content/object.py
@@ -9,16 +9,16 @@ from zope.schema import Text
 class IObject(model.Schema):
     """Dexterity-Schema for Objects"""
 
-    preview = NamedBlobImage(
-        title="Preview",
-        description="Insert an image that will be used in listing and teaser blocks.",
-        required=False,
-    )
-    preview_caption = schema.TextLine(
-        title="Preview image caption",
-        description="",
-        required=False,
-    )
+    # preview = NamedBlobImage(
+    #     title="Preview",
+    #     description="Insert an image that will be used in listing and teaser blocks.",
+    #     required=False,
+    # )
+    # preview_caption = schema.TextLine(
+    #     title="Preview image caption",
+    #     description="",
+    #     required=False,
+    # )
     rawdata = Text(
         title="Rawdata",
         description="",
