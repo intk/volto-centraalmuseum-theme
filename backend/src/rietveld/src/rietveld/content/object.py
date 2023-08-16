70c528c6837b6d5260a16249a1cab816a0e87a51
Cihan Andac <cihanandac@gmail.com>
2023-08-16 12:16:06 +0200

    chores format codes

--- backend/src/rietveld/src/rietveld/content/object.py
+++ backend/src/rietveld/src/rietveld/content/object.py
@@ -8,28 +8,30 @@ from plone.supermodel import model
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from z3c.form.browser.radio import RadioFieldWidget
 from zope.interface import implementer
+from zope.schema import Text
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
-from zope.schema import Text
+
 
 class IObject(model.Schema):
     """Dexterity-Schema for Objects"""
 
     preview = NamedBlobImage(
-        title='Preview',
-        description='Insert an image that will be used in listing and teaser blocks.',
+        title="Preview",
+        description="Insert an image that will be used in listing and teaser blocks.",
         required=False,
     )
     preview_image_caption = schema.TextLine(
-        title= 'Preview image caption',
-        description= '',
-        required= False,
-    )
-    rawdata = Text(
-        title= 'Rawdata',
-        description= '',
+        title="Preview image caption",
+        description="",
         required=False,
     )
+    rawdata = Text(
+        title="Rawdata",
+        description="",
+        required=False,
+    )
+
 
 @implementer(IObject)
 class Object(Container):
