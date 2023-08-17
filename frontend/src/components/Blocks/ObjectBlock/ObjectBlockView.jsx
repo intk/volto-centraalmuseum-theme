4510468a9b3f75f69bdccaa91288ac6de2542d86
Cihan Andac <cihanandac@gmail.com>
2023-08-17 11:32:03 +0200

    Add buttons for the object block

--- frontend/src/components/Blocks/ObjectBlock/ObjectBlockView.jsx
+++ frontend/src/components/Blocks/ObjectBlock/ObjectBlockView.jsx
@@ -2,13 +2,52 @@ import React, { useState } from 'react';
 import { injectIntl } from 'react-intl';
 import './css/objectblock.less';
 import ReactSwipe from 'react-swipe';
-import { BsArrowRight } from 'react-icons/bs';
-import { BsArrowLeft } from 'react-icons/bs';
+import { BsArrowRight, BsArrowLeft } from 'react-icons/bs';
+import { SlMagnifierAdd, SlMagnifierRemove } from 'react-icons/sl';
+import { GoShare, GoDownload } from 'react-icons/go';
+import {
+  TiSocialTwitter,
+  TiSocialFacebook,
+  TiSocialYoutube,
+} from 'react-icons/ti';
+import { SiInstagram } from 'react-icons/si';
 
 const ObjectBlockView = (props) => {
   let reactSwipeEl;
   const [currentIndex, setCurrentIndex] = useState(0);
   const [zoomLevel, setZoomLevel] = useState(1);
+  const [dataExpand, setDataExpand] = useState(false);
+  const currentImageUrl = props.content?.items[currentIndex]?.url;
+  const downloadLink = `${currentImageUrl}/@@images/image`;
+
+  const [popupVisible, setPopupVisible] = useState(false);
+
+  const socialLinks = [
+    {
+      href: 'https://twitter.com/centraalmuseum?lang=en',
+      ariaLabel: 'Share on Twitter',
+      IconComponent: TiSocialTwitter,
+    },
+    {
+      href: 'https://www.facebook.com/centraalmuseum/',
+      ariaLabel: 'Share on Facebook',
+      IconComponent: TiSocialFacebook,
+    },
+    {
+      href: 'https://www.instagram.com/centraalmuseum/',
+      ariaLabel: 'Share on Instagram',
+      IconComponent: SiInstagram,
+    },
+    {
+      href: 'https://www.youtube.com/channel/UCc2cHNtGFZ5pzccEXemfL7g',
+      ariaLabel: 'Share on Youtube',
+      IconComponent: TiSocialYoutube,
+    },
+  ];
+
+  const togglePopup = () => {
+    setPopupVisible(!popupVisible);
+  };
 
   const zoomIn = () => {
     if (zoomLevel < 2) {
@@ -20,6 +59,10 @@ const ObjectBlockView = (props) => {
     if (zoomLevel > 1) {
       setZoomLevel(zoomLevel - 0.2);
     }
+  };
+
+  const expandData = () => {
+    setDataExpand(!dataExpand);
   };
 
   return (
@@ -74,11 +117,69 @@ const ObjectBlockView = (props) => {
             </button>
           </div>
           <div className="buttons">
-            <button onClick={zoomIn}>+</button>
-            <button onClick={zoomOut}>-</button>
+            <button className="button" onClick={expandData}>
+              + Objectgegevens
+            </button>
+            <button className="button share" onClick={togglePopup}>
+              <GoShare
+                icon
+                className="Sharebutton"
+                aria-label="share button"
+                height="2em"
+              />
+              {popupVisible && (
+                <div className="social-media-popup">
+                  {socialLinks.map((link, index) => (
+                    <div key={index}>
+                      <a
+                        aria-label={link.ariaLabel}
+                        data-linktype="external"
+                        target="_blank"
+                        href={link.href}
+                        rel="noreferrer"
+                        data-val={link.href}
+                      >
+                        <link.IconComponent />
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </button>
+            <a
+              className="button"
+              href={downloadLink}
+              role="button"
+              aria-label="download button"
+            >
+              <GoDownload
+                icon
+                className="Downloadbutton"
+                aria-label="download button"
+                height="2em"
+              />
+            </a>
+            <button onClick={zoomIn} className="button">
+              <SlMagnifierAdd
+                icon
+                className="MagnifierPlus"
+                aria-label="magnifier plus"
+                height="2em"
+              />
+            </button>
+            <button onClick={zoomOut} className="button">
+              <SlMagnifierRemove
+                icon
+                className="MagnifierPlus"
+                aria-label="magnifier plus"
+                height="2em"
+              />
+            </button>
           </div>
         </div>
-        <div className="rawdata">{props.content?.rawdata}</div>
+        <div className={`rawdata-section ${dataExpand ? 'expanded' : ''}`}>
+          <p>{props.content?.rawdata}</p>
+        </div>{' '}
       </div>
     </div>
   );
