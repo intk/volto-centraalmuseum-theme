4139b992d66cc96ae31b0584bb28e152f033b4f9
Cihan Andac <cihanandac@gmail.com>
2023-12-12 14:41:24 +0100

    chores

--- backend/src/rietveld/src/rietveld/indexers/indexes.py
+++ backend/src/rietveld/src/rietveld/indexers/indexes.py
@@ -2,10 +2,10 @@ from plone.indexer.decorator import indexer
 from rietveld.content.artwork import IArtwork
 
 
-@indexer(IArtwork)
-def object_remarks(obj):
-    remarks = getattr(obj, "remarks", [])
-    remark_index = []
-    for remark in remarks:
-        remark_index.append(remark)
-    return remark_index
+# @indexer(IArtwork)
+# def object_remarks(obj):
+#     remarks = getattr(obj, "remarks", [])
+#     remark_index = []
+#     for remark in remarks:
+#         remark_index.append(remark)
+#     return remark_index
