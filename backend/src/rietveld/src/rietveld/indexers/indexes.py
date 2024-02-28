a40a442bd98a39099594ec9d9d92d259c3c09ae8
Cihan Andac <cihanandac@gmail.com>
2024-02-28 15:35:01 +0100

    Add artwork_authors index

--- backend/src/rietveld/src/rietveld/indexers/indexes.py
+++ backend/src/rietveld/src/rietveld/indexers/indexes.py
@@ -9,3 +9,16 @@ from rietveld.content.artwork import IArtwork
 #     for remark in remarks:
 #         remark_index.append(remark)
 #     return remark_index
+
+
+@indexer(IArtwork)
+def artwork_author(obj):
+    relations = getattr(obj, "authors", [])
+    titles = []
+    for relation in relations:
+        if relation.isBroken():
+            continue  # Skip broken relations
+        target_object = relation.to_object
+        title = target_object.Title().strip()  # Strip leading and trailing spaces
+        titles.append(title)
+    return titles
