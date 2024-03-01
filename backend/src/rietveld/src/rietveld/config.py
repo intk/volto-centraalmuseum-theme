93573cfa40cbccd8774d32e6ffd9852cc2787591
Cihan Andac <cihanandac@gmail.com>
2024-03-01 09:56:29 +0100

    Add folder paths from the old website

--- backend/src/rietveld/src/rietveld/config.py
+++ backend/src/rietveld/src/rietveld/config.py
@@ -11,6 +11,27 @@ IMPORT_LOCATIONS = {
     # "author_en": "en/collection-research/artists"
     "author": "nl/maker",
     "author_en": "en/creator",
+    # New folder_paths from the old website
+    "intk": "nl/intk",
+    "collect": "nl/collectie",
+    "collect_en": "en/collection",
+    "stadsgeschiedenis": "nl/ontdek/collectie/stadsgeschiedenis",
+    "beeldende kunst 1850 - heden": "nl/ontdek/collectie/moderne-en-hedendaagse-kunst",
+    "kostuums": "nl/ontdek/collectie/mode",
+    "beeldende kunst": "nl/ontdek/collectie/oude-kunst",
+    "prenten en tekeningen": "nl/ontdek/collectie/werken-op-papier-tot-1850",
+    "onedele metalen": "nl/ontdek/collectie/edele-en-onedele-metalen",
+    "edele metalen": "nl/ontdek/collectie/edele-en-onedele-metalen",
+    "beeldhouwkunst tot 1850": "nl/ontdek/collectie/beeldhouwkunst-tot-1850",
+    "schilderkunst tot 1850": "nl/ontdek/collectie/schilderkunst-tot-1850",
+    "meubelen tot 1900": "nl/ontdek/collectie/meubelen-tot-1900",
+    "van baaren": "nl/ontdek/collectie/van-baarencollectie",
+    "kunstnijverheid": "nl/ontdek/collectie/vormgeving",
+    "bruna": "nl/collectie/bruna",
+    "bruna_en": "en/collection/bruna",
+    "rsa": "nl/collectie/rsa",
+    "exhibit": "nl/tentoonstellingen",
 }
+
 
 IMAGE_BASE_URL = "https://cmu.adlibhosting.com/webapiimages/wwwopac.ashx"
