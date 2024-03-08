822a185ecc9de3ef877570edcd9974b500b2bdff
Cihan Andac <cihanandac@gmail.com>
2024-03-08 16:36:11 +0100

    chores

--- backend/src/rietveld/src/rietveld/browser/admin.py
+++ backend/src/rietveld/src/rietveld/browser/admin.py
@@ -1287,10 +1287,10 @@ def get_creator(xml_record):
 
 
 def log_to_file(message):
-    # log_file_path = "/app/logs/collectionLogs.txt"
-    log_file_path = (
-        "/Users/cihanandac/Documents/volto-centraalmuseum-theme/collectionsLogs.txt"
-    )
+    log_file_path = "/app/logs/collectionLogs.txt"
+    # log_file_path = (
+    #     "/Users/cihanandac/Documents/volto-centraalmuseum-theme/collectionsLogs.txt"
+    # )
 
     # Attempt to create the file if it doesn't exist
     try:
