444006ab2436e402a7584712bff817dde0bf1211
Cihan Andac <cihanandac@gmail.com>
2024-03-09 22:34:35 +0100

    Change: the import_object function to exception handling

--- backend/src/rietveld/src/rietveld/browser/admin.py
+++ backend/src/rietveld/src/rietveld/browser/admin.py
@@ -101,8 +101,15 @@ class AdminFixes(BrowserView):
         return trans
 
     def import_objects(
-        self, start=0, limit=10, collection_type="collect", modified_after=""
+        self,
+        full_import=False,
+        start=0,
+        limit=10,
+        collection_type="collect",
+        modified_after="",
     ):
+        MAX_RETRIES = 2
+        DELAY_SECONDS = 1
         counter = 0
         start_time_count = datetime.now()
         start_time = datetime.now().strftime(
@@ -110,29 +117,38 @@ class AdminFixes(BrowserView):
         )  # Record the start time
 
         log_to_file(
-            f"Starting a new batch at {start_time}. start: {start}, end: {limit}"
+            f"Starting a new batch at {start_time}. start: {start}, end: {start + limit}"
         )
 
-        object_priref = self.request.form.get("object_priref")  # 40923
-        headers = "User-Agent: Mozilla/5.0"
-
-        one_hour_before_now = start_time_count - timedelta(hours=1)
-        formatted_one_hour_before_now = one_hour_before_now.strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
-        # if modified_after is '':
-        #     modified_after = self.request.form.get("modified_after", formatted_one_hour_before_now)
+        object_priref = self.request.form.get("object_priref")  #test object priref:40923
 
         if object_priref is not None:
             api_url = f"http://cmu.adlibhosting.com/webapiimages/wwwopac.ashx?database={collection_type}&search=priref={object_priref}"
         else:
-            api_url = f"https://cmu.adlibhosting.com/webapiimages/wwwopac.ashx?database={collection_type}&limit={limit}&startfrom={start}&search=modification greater '{modified_after}'"
-        # else:
-        #     api_url = f"https://cmu.adlibhosting.com/webapiimages/wwwopac.ashx?database={collection_type}&search=priref=*&limit={limit}&startfrom={start}"
+            if full_import:
+                api_url = f"https://cmu.adlibhosting.com/webapiimages/wwwopac.ashx?database={collection_type}&search=priref=*&fields=priref&limit={limit}&startfrom={start}"
+            else:
+                api_url = f"https://cmu.adlibhosting.com/webapiimages/wwwopac.ashx?database={collection_type}&fields=priref&limit={limit}&startfrom={start}&search=modification greater '{modified_after}'"
 
         log_to_file(f"The URL for fetching: {api_url}")
 
-        response = requests.post(api_url)
+        retries = 0
+        success = False
+
+        while retries < MAX_RETRIES and success==False:
+            try:
+                response = requests.post(api_url)
+                success = True
+            except Exception as e:
+                retries += 1
+                if retries < MAX_RETRIES:
+                    time.sleep(DELAY_SECONDS)
+                    log_to_file(f"Temprary failure while fetching API DATA, will try again. Retries {retries}")
+                else:
+                    log_to_file(f"Failure in the batch: start: {start}, end: {start + limit}. Error: {e}")
+                    # TODO: add sending mail for the failed attempt of batch
+                    return
+
         response.raise_for_status()
         api_answer = response.text
         api_answer_bytes = api_answer.encode("utf-8")
@@ -147,23 +163,40 @@ class AdminFixes(BrowserView):
 
         records_tree = records.findall(".//record")
 
-        for tree in records_tree:
+        for record in records_tree:
             transaction.begin()
             log_to_file(f"{counter}. object")
+            retries = 0
+            success = False
 
-            # try:
-            import_one_record(
-                self,
-                tree=tree,
-                container=container,
-                container_en=container_en,
-                catalog=catalog,
-            )
-            transaction.commit()
-            # except Exception as e:
-            #     log_to_file(f"Failure to import the record. Error: {e}")
-            #     transaction.abort()
-            #     break
+            while retries < MAX_RETRIES and success==False:
+                try:
+                    import_one_record(
+                        self,
+                        record=record,
+                        collection_type=collection_type,
+                        container=container,
+                        container_en=container_en,
+                        catalog=catalog,
+                    )
+                    success = True
+                    transaction.commit()
+                except Exception as e:
+                    retries += 1
+                    if retries < MAX_RETRIES:
+                        time.sleep(DELAY_SECONDS)
+                        log_to_file(f"Temprary failure, will try again. Retries {retries}")
+                    else:
+                        log_to_file(f"Failure to import the record. Error: {e}")
+                        transaction.abort()
+                        break
+
+            if not success:
+                log_to_file(
+                    f"Skipped importing object {counter} due to repeated fetching failures."
+                )
+                # TODO: add sending mail for the failed attempt of record
+
             counter = counter + 1
 
         end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -173,7 +206,7 @@ class AdminFixes(BrowserView):
         durationminutes = (duration.total_seconds() % 3600) // 60
         durationseconds = duration.total_seconds() % 60
         log_to_file(
-            f"The sync function ended at {end_time} for the range of objects between {start} and {limit}. It took {durationhours} hour {durationminutes} minutes and {durationseconds} seconds."
+            f"The sync function ended at {end_time} for the range of objects between {start} and {start + limit}. It took {durationhours} hour {durationminutes} minutes and {durationseconds} seconds."
         )
         return "all done"
 
