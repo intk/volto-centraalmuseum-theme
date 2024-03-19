ca3067680baa33436172859d025d6f15a36ad52e
Cihan Andac <cihanandac@gmail.com>
2024-03-19 10:05:09 +0100

    Add table to view for the exhibition fields

--- frontend/src/components/theme/View/ExhibitionView.jsx
+++ frontend/src/components/theme/View/ExhibitionView.jsx
@@ -3,7 +3,7 @@
  * @module components/theme/View/EventView
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   hasBlocksData,
@@ -24,6 +24,136 @@ import {
 } from 'semantic-ui-react';
 import { isEqual } from 'lodash';
 import { getWidget } from '@plone/volto/helpers/Widget/utils';
+import { defineMessages, useIntl } from 'react-intl';
+
+const messages = defineMessages({
+  artist: {
+    id: 'artist',
+    defaultMessage: 'Vervaardiger',
+  },
+  title: {
+    id: 'title',
+    defaultMessage: 'Titel',
+  },
+  materialTechnique: {
+    id: 'materialTechnique',
+    defaultMessage: 'Materiaal / Techniek',
+  },
+  inventoryNumber: {
+    id: 'inventoryNumber',
+    defaultMessage: 'Inventarisnummer',
+  },
+  date: {
+    id: 'date',
+    defaultMessage: 'Datering',
+  },
+  objectExplanation: {
+    id: 'objectExplanation',
+    defaultMessage: 'Fysieke beschrijving',
+  },
+  credit: {
+    id: 'credit',
+    defaultMessage: 'Opmerkingen',
+  },
+  objectNumber: {
+    id: 'objectNumber',
+    defaultMessage: 'Objectnummer',
+  },
+  question: {
+    id: 'question',
+    defaultMessage: 'Vragen?',
+  },
+  questionText: {
+    id: 'questionText',
+    defaultMessage:
+      'Ziet u een fout? Of heeft u extra informatie over dit object? ',
+  },
+  letusknow: {
+    id: 'letusknow',
+    defaultMessage: 'Laat het ons weten!',
+  },
+  share: {
+    id: 'share',
+    defaultMessage: 'Delen',
+  },
+  details: {
+    id: 'details',
+    defaultMessage: 'Objectgegevens',
+  },
+  nowonview: {
+    id: 'nowonview',
+    defaultMessage: 'Nu in het museum',
+  },
+  notonview: {
+    id: 'notonview',
+    defaultMessage: 'Dit object is nu niet in het museum te zien',
+  },
+  objectName: {
+    id: 'objectName',
+    defaultMessage: 'Objectnaam',
+  },
+  acquisition: {
+    id: 'acquisition',
+    defaultMessage: 'Verwerving',
+  },
+  dimensions: {
+    id: 'dimensions',
+    defaultMessage: 'Afmetingen',
+  },
+  inscriptions: {
+    id: 'inscriptions',
+    defaultMessage: 'Opschriften / merken',
+  },
+  category: {
+    id: 'category',
+    defaultMessage: 'Geassocieerd onderwerp',
+  },
+  remarks: {
+    id: 'remarks',
+    defaultMessage: 'Opmerkingen',
+  },
+  documentation: {
+    id: 'documentation',
+    defaultMessage: 'Documentatie',
+  },
+  exhibitions: {
+    id: 'exhibitions',
+    defaultMessage: 'Tentoonstellingen',
+  },
+  physicaldescription: {
+    id: 'physicaldescription',
+    defaultMessage: 'Fysieke beschrijving',
+  },
+  associatedPeriods: {
+    id: 'associatedPeriods',
+    defaultMessage: 'Geassocieerde periode',
+  },
+  associatedPeople: {
+    id: 'associatedPeople',
+    defaultMessage: 'Geassocieerde persoon',
+  },
+  motifs: {
+    id: 'motifs',
+    defaultMessage: 'Motief',
+  },
+  duurzameurl: {
+    id: 'duurzameurl',
+    defaultMessage: 'Duurzame url',
+  },
+  duurzameurltext: {
+    id: 'duurzameurltext',
+    defaultMessage:
+      'Als u naar dit object wilt verwijzen gebruik dan de duurzame URL:',
+  },
+  showmore: {
+    id: 'showmore',
+    defaultMessage: 'Toon alles',
+  },
+  showless: {
+    id: 'showless',
+    defaultMessage: 'Toon minder',
+  },
+});
 
 const translations = {
   expired: {
@@ -78,6 +208,9 @@ const ExhibitionView = (props) => {
   const filteredContent = hasLeadImage
     ? filterBlocks(content, ['title'])
     : content;
+
+  const [showAllDocumentation, setShowAllDocumentation] = useState(false);
+  const intl = useIntl();
 
   // TL;DR: There is a flash of the non block-based view because of the reset
   // of the content on route change. Subscribing to the content change at this
@@ -138,6 +271,140 @@ const ExhibitionView = (props) => {
           ''
         )}
         <RenderBlocks {...props} path={path} content={filteredContent} />
+        <table>
+          <tbody>
+            {content.designer && (
+              <tr>
+                <td className="columnone">
+                  {/* <p>{intl.formatMessage(messages.designer)}</p> */}
+                  <p>Vormgever</p>
+                </td>
+                <td className="columntwo">
+                  <p>{content.designer}</p>
+                </td>
+              </tr>
+            )}
+            {content.documentation && content.documentation?.length !== 0 && (
+              <tr>
+                <td className="columnone">
+                  <p>{intl.formatMessage(messages.documentation)}</p>
+                </td>
+                <td className="columntwo">
+                  {/* {content.documentation.map((document) => (
+                        <p>
+                          <li>{document}</li>
+                        </p>
+                      ))} */}
+                  <ul>
+                    {showAllDocumentation
+                      ? content?.documentation?.map((doc, index) => (
+                          <li>
+                            <p key={index}>
+                              {doc}
+                              {index === 2 && content.documentation.length > 3 && (
+                                <button
+                                  className={`expand-data-button ${showAllDocumentation}`}
+                                  onClick={() =>
+                                    setShowAllDocumentation(
+                                      !showAllDocumentation,
+                                    )
+                                  }
+                                >
+                                  {`${intl.formatMessage(messages.showless)} -`}
+                                </button>
+                              )}
+                            </p>
+                          </li>
+                        ))
+                      : content.documentation
+                          ?.slice(0, 3)
+                          ?.filter((el) => el.trim() !== '')
+                          .map((doc, index) => (
+                            <li>
+                              <p key={index}>
+                                {doc}
+                                {index === 2 &&
+                                  content.documentation.length > 3 && (
+                                    <button
+                                      className="expand-data-button"
+                                      onClick={() =>
+                                        setShowAllDocumentation(
+                                          !showAllDocumentation,
+                                        )
+                                      }
+                                    >
+                                      {/* Toon alles + */}
+                                      {`${intl.formatMessage(
+                                        messages.showmore,
+                                      )} +`}
+                                    </button>
+                                  )}
+                              </p>
+                            </li>
+                          ))}
+                  </ul>
+                </td>
+              </tr>
+            )}
+            {content.objects && (
+              <tr>
+                <td className="columnone">
+                  {/* <p>{intl.formatMessage(messages.materialTechnique)}</p> */}
+                  Collectie in deze tentoonstelling
+                </td>
+                <td className="columntwo">
+                  <p>
+                    {content?.objects.map(({ title, url }, index) => (
+                      <>
+                        <span>
+                          <a href={`/${url[2]}/${url[3]}/${url[4]}/${url[5]}`}>
+                            {title}
+                          </a>
+                        </span>
+                        <span>
+                          {index !== content.objects.length - 1 ? ', ' : ''}
+                        </span>
+                      </>
+                    ))}
+                  </p>
+                </td>
+              </tr>
+            )}
+            {content.persistent_url && (
+              <tr>
+                <td className="columnone">
+                  <p>{intl.formatMessage(messages.duurzameurl)}</p>
+                </td>
+                <td className="columntwo">
+                  <p>
+                    <p>{intl.formatMessage(messages.duurzameurltext)}</p>
+                    <a href={content.persistent_url}>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: content.persistent_url,
+                        }}
+                      />
+                    </a>
+                  </p>
+                </td>
+              </tr>
+            )}
+            <tr>
+              <td className="columnone">
+                <p>{intl.formatMessage(messages.question)}</p>{' '}
+              </td>
+              <td className="columntwo">
+                <p>
+                  {intl.formatMessage(messages.questionText)}
+                  <span> </span>
+                  <a href="mailto:documentatie@centraalmuseum.nl?subject=opmerking%20over%20object:%2010786">
+                    {intl.formatMessage(messages.letusknow)}
+                  </a>
+                </p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </Container>
     ) : (
       <Container id="page-document">
