776d2c3ab3d91c145f468a435d2022599cc451c6
Cihan Andac <cihanandac@gmail.com>
2024-03-19 15:17:02 +0100

    chores

--- frontend/src/components/theme/View/ExhibitionView.jsx
+++ frontend/src/components/theme/View/ExhibitionView.jsx
@@ -286,7 +286,7 @@ const ExhibitionView = (props) => {
                   </td>
                 </tr>
               )}
-              {content.objects && (
+              {content.objects && content.objects?.length !== 0 && (
                 <tr>
                   <td className="columnone">
                     <p>{intl.formatMessage(messages.objects)}</p>
