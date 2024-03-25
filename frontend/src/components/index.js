a0e0a457dc41392ca6fa20f9cca8b666063fec9a
Cihan Andac <cihanandac@gmail.com>
2024-03-25 12:40:50 +0100

    chores

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -12,5 +12,6 @@
 import SeeMore from './theme/SeeMore/SeeMore';
 import ArtworkPreview from './theme/ArtworkPreview/ArtworkPreview';
 import SeeMoreAuthor from './theme/SeeMore/SeeMoreAuthor';
+import ImageAlbum from './theme/ImageAlbum/ImageAlbum';
 
-export { SeeMore, ArtworkPreview, SeeMoreAuthor };
+export { SeeMore, ArtworkPreview, SeeMoreAuthor, ImageAlbum };
