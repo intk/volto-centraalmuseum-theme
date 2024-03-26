a4aafa14901a4fe068ac0be1c72b8aba8f17dee5
Cihan Andac <cihanandac@gmail.com>
2024-03-26 15:16:04 +0100

    chores

--- backend/src/rietveld/src/rietveld/browser/admin.py
+++ backend/src/rietveld/src/rietveld/browser/admin.py
@@ -1359,7 +1359,7 @@ def import_one_exhibition(
 
     designer = artwork_creator
 
-    if "vormgever" in artwork_creator_role.lower() :
+    if "vormgever" in artwork_creator_role.lower():
         designer = artwork_vermelding
 
     persistent_url = tree.findtext(".//PIDwork/PID_work_URI")
