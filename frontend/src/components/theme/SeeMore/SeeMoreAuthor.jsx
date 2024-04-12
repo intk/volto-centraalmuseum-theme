3ca3ed0425f5fa9338175842fc12eb450caa4536
Cihan Andac <cihanandac@gmail.com>
2024-04-12 10:27:59 +0200

    Changed: batch size to 20

--- frontend/src/components/theme/SeeMore/SeeMoreAuthor.jsx
+++ frontend/src/components/theme/SeeMore/SeeMoreAuthor.jsx
@@ -48,7 +48,7 @@ const Search = (props) => {
       artwork_author: authorQueryString,
       path: currentPath,
       metadata_fields: ['dating'],
-      b_size: 1000,
+      b_size: 20,
     };
     searchContent('', options);
   };
