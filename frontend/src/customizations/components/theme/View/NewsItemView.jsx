7adc43dc97e430243cb7e08bdb0ec32175414188
Cihan Andac <cihanandac@gmail.com>
2024-04-15 00:04:36 +0200

    Add blog writer image, title and link to the view

--- frontend/src/customizations/components/theme/View/NewsItemView.jsx
+++ frontend/src/customizations/components/theme/View/NewsItemView.jsx
@@ -3,7 +3,8 @@
  * @module components/theme/View/NewsItemView
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Container as SemanticContainer, Image } from 'semantic-ui-react';
 import {
@@ -13,6 +14,9 @@ import {
 } from '@plone/volto/helpers';
 import RenderBlocks from '@plone/volto/components/theme/View/RenderBlocks';
 import config from '@plone/volto/registry';
+import { useIntl } from 'react-intl';
+import { searchContent } from '@plone/volto/actions';
+import { Link } from 'react-router-dom';
 
 /**
  * NewsItemView view component class.
@@ -21,11 +25,59 @@ import config from '@plone/volto/registry';
  * @returns {string} Markup of the component.
  */
 const NewsItemView = ({ content }) => {
+  const [blogWriter, setBlogWriter] = useState();
+  const dispatch = useDispatch();
+  const intl = useIntl();
   const Container =
     config.getComponent({ name: 'Container' }).component || SemanticContainer;
 
+  useEffect(() => {
+    const doSearch = () => {
+      const currentPath = intl.locale; // Adjust the path as necessary
+      const options = {
+        portal_type: 'blogwriter',
+        blogWriterID: content?.creators[0].toLowerCase(),
+        path: currentPath,
+        metadata_fields: ['title', 'description'],
+      };
+
+      dispatch(searchContent('', options))
+        .then((response) => {
+          if (response?.items?.length > 0) {
+            setBlogWriter(response?.items[0]);
+          } else {
+            setBlogWriter([]);
+          }
+        })
+        .catch((error) => {
+          setBlogWriter([]);
+        });
+    };
+    doSearch();
+  }, [content, dispatch, intl]);
+
   return hasBlocksData(content) ? (
     <Container id="page-document" className="view-wrapper newsitem-view">
+      {blogWriter && (
+        <div className="blog-writer" key={blogWriter['@id']}>
+          {blogWriter.image_field && (
+            <div className="writer-image-wrapper">
+              <Link to={flattenToAppURL(blogWriter?.['@id'])}>
+                <img
+                  src={`${blogWriter?.['@id']}/${blogWriter?.image_scales?.preview_image[0]?.download}`}
+                  alt="writer"
+                ></img>
+              </Link>
+            </div>
+          )}
+          <Link to={flattenToAppURL(blogWriter?.['@id'])}>
+            <h1 className="writer-title">{blogWriter?.title}</h1>
+          </Link>
+          {blogWriter?.description && (
+            <p className="writer-description">{blogWriter.description}</p>
+          )}
+        </div>
+      )}
       <RenderBlocks content={content} />
     </Container>
   ) : (
