86a4b53aeabd5ddd745eee8d6b0ec38f0b960ec9
Cihan Andac <cihanandac@gmail.com>
2024-04-15 00:06:51 +0200

    Add: see more section for the blog writer

--- frontend/src/components/theme/View/BlogWriterView.jsx
+++ frontend/src/components/theme/View/BlogWriterView.jsx
@@ -3,6 +3,7 @@ import { Container } from 'semantic-ui-react';
 import './css/blogwriterview.less';
 import { BodyClass } from '@plone/volto/helpers';
 import { flattenToAppURL } from '@plone/volto/helpers';
+import { SeeMoreNewsItem } from '../../index';
 
 export default function BlogWriterView(props) {
   const src = props.content.preview_image
@@ -12,22 +13,24 @@ export default function BlogWriterView(props) {
   return (
     <div id="object-block">
       <BodyClass className="hide-top-image" />
-      {props.content.title && (
+      {props?.content?.title && (
         <div className="description-wrapper">
           <p className="documentDescription author">
-            {props.content.preview_image && (
+            {props?.content?.preview_image && (
               <div className="writer-image-wrapper">
                 <img src={src} alt="writer"></img>
               </div>
             )}
-            {props.content.title}
+            {props?.content?.title}
           </p>
-          {props.content.description && (
+          {props?.content?.description && (
             <p className="author_url">{props.content.description}</p>
           )}
         </div>
       )}
-      <Container></Container>
+      <Container>
+        <SeeMoreNewsItem {...props} />
+      </Container>
     </div>
   );
 }
