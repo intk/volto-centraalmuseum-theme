9f816e26c6fc7e04f4063a4fb531dd6a3872648c
Cihan Andac <cihanandac@gmail.com>
2024-04-15 10:50:50 +0200

    chores

--- frontend/src/components/theme/View/AuthorView.jsx
+++ frontend/src/components/theme/View/AuthorView.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-// import { Container } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './css/authorview.less';
-// import { SeeMoreAuthor } from '../../index';
-// import { defineMessages, useIntl } from 'react-intl';
+import { SeeMoreAuthor } from '../../index';
+import { defineMessages, useIntl } from 'react-intl';
 
-// const messages = defineMessages({
-//   authorUrl: {
-//     id: 'authorUrl',
-//     defaultMessage:
-//       'Bekijk hier alle werken van {authorName} in de collectie van Centraal Museum. Voor kunsthistorische documentatie over {authorName}, ga naar',
-//   },
-// });
+const messages = defineMessages({
+  authorUrl: {
+    id: 'authorUrl',
+    defaultMessage:
+      'Bekijk hier alle werken van {authorName} in de collectie van Centraal Museum. Voor kunsthistorische documentatie over {authorName}, ga naar',
+  },
+});
 
 export default function AuthorView(props) {
-  // const intl = useIntl();
+  const intl = useIntl();
   return (
     <div id="object-block">
-      {/* {props.content.title && (
+      {props.content.title && (
         <div className="description-wrapper">
           <p className="documentDescription author">{props.content.title}</p>
 
@@ -31,7 +31,7 @@ export default function AuthorView(props) {
       )}
       <Container>
         <SeeMoreAuthor {...props} />
-      </Container> */}
+      </Container>
     </div>
   );
 }
