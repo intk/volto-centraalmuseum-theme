110636c124995872b009efe7f06a3894d9ed4bd3
Cihan Andac <cihanandac@gmail.com>
2024-04-18 14:49:54 +0200

    Add imageviewfullscreen view

---
+++ frontend/src/components/theme/ImageViewFullscreen/ImageViewFullscreen.jsx
@@ -0,0 +1,138 @@
+/**
+ * Search component.
+ * @module components/theme/Search/Search
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+// eslint-disable-next-line no-unused-vars
+import { Container, Pagination, Button, Header } from 'semantic-ui-react';
+// import classNames from 'classnames';
+import { defineMessages, injectIntl } from 'react-intl';
+import { searchContent } from '@plone/volto/actions';
+// eslint-disable-next-line no-unused-vars
+import { SearchTags, Toolbar, Icon } from '@plone/volto/components';
+import { Link } from 'react-router-dom';
+import './css/ImageViewFullscreen.less';
+
+const messages = defineMessages({
+  imagepurpose: {
+    id: 'imagepurpose',
+    defaultMessage:
+      'Gebruik voor studie of privédoeleinden. Beeldrecht is van toepassing. Aanvragen voorwaarden copyright en/of hogere resolutie:',
+  },
+  include: {
+    id: 'include',
+    defaultMessage: 'Bij gebruik deze gegevens vermelden: ',
+  },
+  mercis: {
+    id: 'mercis',
+    defaultMessage: 'Mercis Publishing/Centraal Museum, Utrecht',
+  },
+  bestelformulier: {
+    id: 'bestelformulier',
+    defaultMessage: 'Bestelformulier',
+  },
+});
+
+// const test = withQuerystringResults(this.props);
+// console.log(test);
+
+/**
+ * Search class.
+ * @class SearchComponent
+ * @extends Component
+ */
+class ImageViewFullscreen extends Component {
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    subject: PropTypes.string,
+    path: PropTypes.string,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        '@id': PropTypes.string,
+        '@type': PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+      }),
+    ),
+    pathname: PropTypes.string.isRequired,
+  };
+
+  /**
+   * Default properties.
+   * @property {Object} defaultProps Default properties.
+   * @static
+   */
+  static defaultProps = {
+    items: [],
+    searchableText: null,
+    subject: null,
+    path: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    const { intl } = this.props;
+    const imagepath = `${this.props.pathname
+      .split('/')
+      .slice(0, -1)
+      .join('/')}/@@images/preview_image/great`;
+
+    return (
+      <Container id="page-search">
+        <div className="home-link">
+          <Link to={`/${intl.locale}`}>Home</Link>
+        </div>
+        <p>
+          {this.props.intl.formatMessage(messages.imagepurpose)}{' '}
+          {this.props.intl.formatMessage(messages.bestelformulier)}
+        </p>
+        <p>
+          {this.props.intl.formatMessage(messages.include)}{' '}
+          <i>{this.props.intl.formatMessage(messages.mercis)}</i>
+        </p>
+        <div className="image-section">
+          <img src={imagepath} alt="artwork"></img>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export const __test__ = compose(
+  injectIntl,
+  connect(
+    (state, props) => ({
+      pathname: props.history.location.pathname,
+    }),
+    { searchContent },
+  ),
+)(ImageViewFullscreen);
+
+export default compose(
+  injectIntl,
+  connect(
+    (state, props) => ({
+      items: state.search.items,
+      pathname: props.location.pathname,
+      currentLang: state.intl?.locale,
+    }),
+    { searchContent },
+  ),
+)(ImageViewFullscreen);
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,6 +5,8 @@
 
 import { defaultRoutes } from '@plone/volto/routes';
 import config from '@plone/volto/registry';
+import ImageViewFullscreen from '@package/components/theme/ImageViewFullscreen/ImageViewFullscreen';
+// import { Search } from '@plone/volto/components';
 
 /**
  * Routes array.
@@ -14,8 +16,13 @@ import config from '@plone/volto/registry';
 const routes = [
   {
     path: '/',
-    component: config.getComponent('App').component, // Change this if you want a different component
+    component: config.getComponent('App').component,
     routes: [
+      {
+        path: '/*.(jpg|jpeg|tif)/image_view_fullscreen',
+        // path: '/**/image_view_fullscreen',
+        component: ImageViewFullscreen,
+      },
       // Add your routes here
       ...(config.addonRoutes || []),
       ...defaultRoutes,
