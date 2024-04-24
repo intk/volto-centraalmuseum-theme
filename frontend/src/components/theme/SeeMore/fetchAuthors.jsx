ebaa5d4963a5a1e17d72a3b8f4f2dacf110fa591
Cihan Andac <cihanandac@gmail.com>
2024-04-24 13:45:17 +0200

    chores

--- frontend/src/components/theme/SeeMore/fetchAuthors.jsx
+++ frontend/src/components/theme/SeeMore/fetchAuthors.jsx
@@ -1,26 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { searchContent } from '@plone/volto/actions';
 import { Link } from 'react-router-dom';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
 const FetchAuthor = ({ item }) => {
-  const [authorDetails, setAuthorDetails] = useState(); // Initialize with item to ensure all data is preserved
-  const dispatch = useDispatch();
+  const [authorDetails, setAuthorDetails] = useState();
 
   useEffect(() => {
     const fetchBlogWriterData = async () => {
       if (!item?.Creator) return;
 
       const params = new URLSearchParams({
-        // portal_type: 'blogwriter',
         blogWriterID: item.Creator.toLowerCase(),
-        // metadata_fields: JSON.stringify(['title', 'description']),
       });
-
-      {
-        console.log(`/++api++/nl/search_blogwriters?${params.toString()}`);
-      }
 
       try {
         const response = await fetch(
@@ -40,7 +31,7 @@ const FetchAuthor = ({ item }) => {
           setAuthorDetails(data[0]);
         }
       } catch (error) {
-        console.error('Error fetching blog writer data:', error);
+        // console.error('Error fetching blog writer data:', error);
       }
     };
 
@@ -49,7 +40,6 @@ const FetchAuthor = ({ item }) => {
 
   return (
     <div>
-      {console.log(authorDetails)}
       {authorDetails && (
         <div className="blog-writer">
           {authorDetails.preview_image_url && (
