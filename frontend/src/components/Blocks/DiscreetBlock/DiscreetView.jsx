a82c74fab81cc1f73a3920e9a7d26e5f72580ae0
Cihan Andac <cihanandac@gmail.com>
2024-05-03 14:51:07 +0200

    chores

--- frontend/src/components/Blocks/DiscreetBlock/DiscreetView.jsx
+++ frontend/src/components/Blocks/DiscreetBlock/DiscreetView.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const DiscreetView = ({ data, mode = 'view' }) => {
   return (
     <>
-      {console.log(data)}
       {data?.text ? (
         <p
           className="callout"
